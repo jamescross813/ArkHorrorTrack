8de8f5f441f13e40986fdfa102bd1a0903d54039
jamescross <crossjames813@gmail.com>
2021-07-10 00:48:47 -0500

    add scenario attributes for nested form

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,4 +3,16 @@ class Game < ApplicationRecord
   has_many :characters
   has_many :character_bases, through: :characters
   has_many :scenarios
+  accepts_nested_attributes_for :scenarios
+  
+  
+    def scenarios_attributes=(scenarios_attributes)
+      scenarios_attributes.values.each do |scenarios_attribute|
+          scenario = Scenario.create(scenarios_attribute)          
+            self.scenarios << scenario
+      end
+    end
+
+    
+
 end
