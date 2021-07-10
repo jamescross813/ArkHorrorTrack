0ad610c87fb3f971ab7b29cb76486eee398d91d2
jamescross <crossjames813@gmail.com>
2021-07-10 00:51:39 -0500

    session controller new and create actions, with params

--- app/controllers/scenarios_controller.rb
+++ app/controllers/scenarios_controller.rb
@@ -1,2 +1,21 @@
 class ScenariosController < ApplicationController
+
+    def new
+        @scenario = Scenario.new(game_id: params[:game_id])
+    end
+
+    def create 
+        @scenario = Scenario.new(scenario_params)
+        if @scenario.save
+            redirect_to game_scenario_path(@scenario.game_id, @scenario)
+        else
+            render :new
+        end
+    end
+
+    private 
+
+    def scenario_params
+        params.require(:scenario).permit(:name, :order_number, :completion_status, :game_id, :run_number)
+    end
 end
