6504cb245e248f48bc7de308c7d9be0661341d07
jamescross <crossjames813@gmail.com>
2021-07-09 22:38:54 -0500

    user new and create actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+
+    def new    
+    end
+
+    def create
+        @user = User.find_by(username: params[:username])
+        if @user
+          return head(:forbidden) unless @user.authenticate(params[:password])
+          session[:user_id] = @user.id
+          redirect_to user_path(@user)
+        else 
+          redirect_to login_path
+        end
+        
+    end
 end
