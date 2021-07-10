0a76300c0c69cbdf07e9a62aab4a80e073e7acb7
jamescross <crossjames813@gmail.com>
2021-07-10 01:11:40 -0500

    new and create action for notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,21 @@
 class NotesController < ApplicationController
+
+    def new
+        @note = Note.new(scenario_id: params[:scenario_id])
+    end
+
+    def create 
+        @note = Note.new(note_params)
+        if @note.save
+           redirect_to scenario_note_path(@note.scenario_id, @note)
+        else 
+            render :new
+        end
+    end
+
+    private
+
+    def note_params
+        params.require(:note).permit(:title, :content, :scenario_id)
+    end
 end
