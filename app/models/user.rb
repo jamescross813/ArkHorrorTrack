475d2e5768b02f76c393456cb2026592c47ff048
jamescross <crossjames813@gmail.com>
2021-07-10 02:07:52 -0500

    user  validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
     has_many :games
+
     has_secure_password
+
+    validates :username, uniqueness: true, presence: true
+    validates :password_digest, presence:true
+    
 end
