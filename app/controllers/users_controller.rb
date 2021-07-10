5474da259e68c9164df0998bb1e28557da96fffe
jamescross <crossjames813@gmail.com>
2021-07-09 22:48:33 -0500

    correction to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,16 @@
 class UsersController < ApplicationController
 
     def new    
+        @user = User.new
     end
 
     def create
-        @user = User.find_by(username: params[:username])
-        if @user
-          return head(:forbidden) unless @user.authenticate(params[:password])
-          session[:user_id] = @user.id
-          redirect_to user_path(@user)
-        else 
-          redirect_to login_path
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            render :show
+        else
+            render :new
         end 
     end
 end
