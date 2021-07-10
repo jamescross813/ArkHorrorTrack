6270e1c0e3c33a313fc3a39b6b28a76dc764f7d9
jamescross <crossjames813@gmail.com>
2021-07-10 02:23:20 -0500

    character validation added

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,4 +1,6 @@
 class Character < ApplicationRecord
   belongs_to :game
   belongs_to :character_base
+
+  validates :health, :sanity, :victory_points_gained, :victory_points_used, format: {without: /a-z/, message: "Must be a number"}
 end
