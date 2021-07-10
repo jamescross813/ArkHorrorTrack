4d78db37280cb7adc5a3d596be20f15beea407ad
jamescross <crossjames813@gmail.com>
2021-07-09 22:06:20 -0500

    column title changes

--- db/migrate/20210710030358_create_notes.rb
+++ db/migrate/20210710030358_create_notes.rb
@@ -2,8 +2,8 @@ class CreateNotes < ActiveRecord::Migration[6.1]
   def change
     create_table :notes do |t|
       t.belongs_to :scenario, null: false, foreign_key: true
-      t.title :sting
-      t.content :text
+      t.string :title 
+      t.text :content
 
       t.timestamps
     end
