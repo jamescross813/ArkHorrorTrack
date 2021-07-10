63b326be706ecd0dfc867e795f32c0559039885a
jamescross <crossjames813@gmail.com>
2021-07-10 12:57:39 -0500

    validations for user and login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,8 @@ class UsersController < ApplicationController
 
     def create
         @user = User.new(user_params)
-        if @user.save
+        if @user.valid?
+            @user.save
             session[:user_id] = @user.id
             render :show
         else
