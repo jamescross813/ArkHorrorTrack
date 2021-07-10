a6e9d7dbba6d6199ac13a84b3c7f0b6a00dd936c
jamescross <crossjames813@gmail.com>
2021-07-10 00:31:48 -0500

    added new, create actions and params

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,23 @@
 class GamesController < ApplicationController
+
+    def new
+        @game = Game.new(user_id: params[:user_id])
+        @game.scenarios.build
+    end
+
+    def create 
+        @game = Game.new(game_params)
+        if @game.save 
+           redirect_to user_game_path(@game.user_id, @game)
+        else 
+            render :new
+        end
+    end
+
+    def game_params
+        params.require(:game).permit(:title, :order, :user_id, :scenario_ids => [], :scenarios_attributes=> [:title,
+                                                                                                            :order_number,
+                                                                                                            :completion_status,
+                                                                                                            :run_number])
+    end
 end
