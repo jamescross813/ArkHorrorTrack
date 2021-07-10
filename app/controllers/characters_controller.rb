30d6bf448e2287d945ab17e19a640ce18acef2ab
jamescross <crossjames813@gmail.com>
2021-07-10 01:02:31 -0500

    new, create actions for character and params

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,2 +1,21 @@
 class CharactersController < ApplicationController
+
+    def new
+        @gamecharacter = Character.new(game_id: params[:game_id])
+    end
+
+    def create 
+        @character = Character.new(character_params)
+        if @character.save 
+           redirect_to game_character_path(@character.game_id, @character)
+        else 
+            render :new
+        end
+    end
+
+    private 
+
+    def character_params
+        params.require(:character).permit(:character_base_id, :game_id, :victory_points_gained, :victory_points_used, :health, :sanity)
+    end
 end
