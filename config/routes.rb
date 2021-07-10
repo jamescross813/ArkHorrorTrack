e9c0777b8e04e368e6b6ad2022e3088e5969ae36
jamescross <crossjames813@gmail.com>
2021-07-09 22:33:22 -0500

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,34 @@
 Rails.application.routes.draw do
-  resources :character_bases
-  resources :users
+  root to: 'application#homepage'
+  
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+  get '/login' => 'sessions#new' 
+  post '/login' => 'sessions#create' 
+  post '/logout' => 'sessions#destroy'
+  
+  get '/auth/twitter/callback' => 'sessions#create_twitter' 
+
   resources :scenarios
-  resources :notes
-  resources :games
   resources :characters
+  resources :users
+  resources :notes
+  resources :character_bases, only:[:show, :index]
+  
+  resources :users do
+    resources :games
+  end
+  resources :games do 
+    resources :characters
+  end
+  resources :games do 
+    resources :scenarios
+  end  
+
+  resources :scenarios do 
+    resources :notes
+  end
+  
+  match '/auth/:provider/callback', to: 'auth#callback', via: [:get, :post]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
