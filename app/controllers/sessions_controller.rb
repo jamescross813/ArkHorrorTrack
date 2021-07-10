e73c8c88e8665bbeee9b25620948f1fde1a4ca22
jamescross <crossjames813@gmail.com>
2021-07-10 11:37:23 -0500

    changed if statement for log on

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,7 @@ class SessionsController < ApplicationController
 
     def create
         @user = User.find_by(username: params[:username])
-        if @user
-          return head(:forbidden) unless @user.authenticate(params[:password])
+        if @user && @user.authenticate(params[:password])
           session[:user_id] = @user.id
           redirect_to user_path(@user)
         else 
